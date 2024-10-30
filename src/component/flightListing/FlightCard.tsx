6f7962fc31709d4043142376b7aa275a35dc31ed
zakariyahrobiat <zakariyahrobiat2018@gmail.com>
2024-10-30 15:06:04 +0100

    flight filter section

--- src/component/flightListing/FlightCard.tsx
+++ src/component/flightListing/FlightCard.tsx
@@ -1,6 +1,89 @@
+
+import { useAuth } from "../../hooks/useAuth"
+const data =[
+{
+  id:1,
+  name:"HorizonJet",
+  time:"10:25PM - 7:06AM",
+  hours:"10 hr 41 min",
+  price:275.5,
+  type:"1 stop"
+},
+{
+  id:2,
+  name:"Altitude Airways",
+  time:"6:30AM - 7:55AM",
+  hours:"3 hr 25 min",
+  price:206,
+  type:"1 stop"
+},
+{
+  id:3,
+  name:"Cloudy Airlines",
+  time:"6:30AM - 7:55AM",
+  hours:"3 hr 25 min",
+  price:206,
+  type:"1 stop"
+}
+,
+{
+  id:4,
+  name:"Altitude Airways",
+  time:"6:30AM - 7:55AM",
+  hours:"3 hr 25 min",
+  price:206,
+  type:"1 stop"
+}
+,
+{
+  id:5,
+  name:"Altitude Airways",
+  time:"6:30AM - 7:55AM",
+  hours:"3 hr 25 min",
+  price:206,
+  type:"1 stop"
+}
+,
+{
+  id:6,
+  name:"Altitude Airways",
+  time:"6:30AM - 7:55AM",
+  hours:"3 hr 25 min",
+  price:206,
+  type:"1 stop"
+}
+,
+{
+  id:7,
+  name:"Altitude Airways",
+  time:"6:30AM - 7:55AM",
+  hours:"3 hr 25 min",
+  price:206,
+  type:"1 stop"
+}
+,
+{
+  id:8,
+  name:"Altitude Airways",
+  time:"6:30AM - 7:55AM",
+  hours:"3 hr 25 min",
+  price:206,
+  type:"1 stop"
+}
+]
 const FlightCard = () => {
+ const {dates}= useAuth()
   return (
-    <div>FlightCard</div>
+<div className="mt-5">
+  <div className="flex justify-between items-center bg-primary-600 px-2">
+  {dates.map((item)=>{
+    return(
+      <div className="text-white p-2 active:bg-white active:text-primary-600 active:rounded-t-md text-sm font-medium mt-2"><button > {item.toDateString()}</button></div>
+    )
+   
+})}
+  </div>
+</div>
   )
 }
 
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,9 +1,28 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
 import { PropsWithChildren } from "react";
 interface AppContextType{
-
+dates:Date[]
 }
-export const AppContext = createContext<AppContextType>({})
+export const AppContext = createContext<AppContextType>({
+   dates:[]
+})
 export const Context =(props:PropsWithChildren)=>{
-   return  <AppContext.Provider value={{}}>{props.children}</AppContext.Provider> 
+   const [dates, setDate] = useState <Date[]>([])
+   const generateDates = (): Date[]=>{
+     const dates: Date[]=[]
+     const today = new Date()
+     for (let i =0; i<5; i++){
+       const newDate = new Date(today);
+ newDate.setDate(today.getDate() + i);
+ dates.push(newDate)
+     }
+     return dates
+   }
+ 
+   useEffect(()=>{
+     setDate(generateDates())
+   },[])
+   return  <AppContext.Provider value={{
+      dates:dates
+   }}>{props.children}</AppContext.Provider> 
 }
