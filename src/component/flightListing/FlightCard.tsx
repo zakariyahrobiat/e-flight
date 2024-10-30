0fa64fae108494f72dbb0a7852ef84c745262dfe
zakariyahrobiat <zakariyahrobiat2018@gmail.com>
2024-10-30 15:16:45 +0100

    build error

--- src/component/flightListing/FlightCard.tsx
+++ src/component/flightListing/FlightCard.tsx
@@ -1,76 +1,76 @@
 
 import { useAuth } from "../../hooks/useAuth"
-const data =[
-{
-  id:1,
-  name:"HorizonJet",
-  time:"10:25PM - 7:06AM",
-  hours:"10 hr 41 min",
-  price:275.5,
-  type:"1 stop"
-},
-{
-  id:2,
-  name:"Altitude Airways",
-  time:"6:30AM - 7:55AM",
-  hours:"3 hr 25 min",
-  price:206,
-  type:"1 stop"
-},
-{
-  id:3,
-  name:"Cloudy Airlines",
-  time:"6:30AM - 7:55AM",
-  hours:"3 hr 25 min",
-  price:206,
-  type:"1 stop"
-}
-,
-{
-  id:4,
-  name:"Altitude Airways",
-  time:"6:30AM - 7:55AM",
-  hours:"3 hr 25 min",
-  price:206,
-  type:"1 stop"
-}
-,
-{
-  id:5,
-  name:"Altitude Airways",
-  time:"6:30AM - 7:55AM",
-  hours:"3 hr 25 min",
-  price:206,
-  type:"1 stop"
-}
-,
-{
-  id:6,
-  name:"Altitude Airways",
-  time:"6:30AM - 7:55AM",
-  hours:"3 hr 25 min",
-  price:206,
-  type:"1 stop"
-}
-,
-{
-  id:7,
-  name:"Altitude Airways",
-  time:"6:30AM - 7:55AM",
-  hours:"3 hr 25 min",
-  price:206,
-  type:"1 stop"
-}
-,
-{
-  id:8,
-  name:"Altitude Airways",
-  time:"6:30AM - 7:55AM",
-  hours:"3 hr 25 min",
-  price:206,
-  type:"1 stop"
-}
-]
+// const data =[
+// {
+//   id:1,
+//   name:"HorizonJet",
+//   time:"10:25PM - 7:06AM",
+//   hours:"10 hr 41 min",
+//   price:275.5,
+//   type:"1 stop"
+// },
+// {
+//   id:2,
+//   name:"Altitude Airways",
+//   time:"6:30AM - 7:55AM",
+//   hours:"3 hr 25 min",
+//   price:206,
+//   type:"1 stop"
+// },
+// {
+//   id:3,
+//   name:"Cloudy Airlines",
+//   time:"6:30AM - 7:55AM",
+//   hours:"3 hr 25 min",
+//   price:206,
+//   type:"1 stop"
+// }
+// ,
+// {
+//   id:4,
+//   name:"Altitude Airways",
+//   time:"6:30AM - 7:55AM",
+//   hours:"3 hr 25 min",
+//   price:206,
+//   type:"1 stop"
+// }
+// ,
+// {
+//   id:5,
+//   name:"Altitude Airways",
+//   time:"6:30AM - 7:55AM",
+//   hours:"3 hr 25 min",
+//   price:206,
+//   type:"1 stop"
+// }
+// ,
+// {
+//   id:6,
+//   name:"Altitude Airways",
+//   time:"6:30AM - 7:55AM",
+//   hours:"3 hr 25 min",
+//   price:206,
+//   type:"1 stop"
+// }
+// ,
+// {
+//   id:7,
+//   name:"Altitude Airways",
+//   time:"6:30AM - 7:55AM",
+//   hours:"3 hr 25 min",
+//   price:206,
+//   type:"1 stop"
+// }
+// ,
+// {
+//   id:8,
+//   name:"Altitude Airways",
+//   time:"6:30AM - 7:55AM",
+//   hours:"3 hr 25 min",
+//   price:206,
+//   type:"1 stop"
+// }
+// ]
 const FlightCard = () => {
  const {dates}= useAuth()
   return (
