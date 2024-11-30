e29cad761b83303664a020dc9db7c5121d9270c6
zakariyahrobiat <zakariyahrobiat2018@gmail.com>
2024-11-30 19:36:16 +0100

    flight ticket mobile page

--- src/component/bookingDetails/FlightTicket.tsx
+++ src/component/bookingDetails/FlightTicket.tsx
@@ -27,7 +27,7 @@ fontSize = 8}:BarcodeProps) => {
       if (svgRef.current) {
         JsBarcode(svgRef.current, value, {
           format,
-          width,
+          width:0.8,
           height,
           displayValue,
           lineColor,
@@ -50,46 +50,46 @@ fontSize = 8}:BarcodeProps) => {
   const {detail, input}= useAuth()
   const {start, travelClass, number, end} = input
   return (
-    <div>
+    <div className="w-full">
      
-      <div className="flex justify-between gap-10 bg-slate-100  pl-10 my-5 w-full rounded-xl">
-        <div className="flex justify-between py-5  w-2/3">
-        <div>
+      <div className="flex justify-between gap-2 md:gap-10 bg-slate-100 pl-2 md:pl-10 my-2 md:my-5 rounded-xl">
+        <div className="flex justify-between items-center py-2 md:py-5  w-2/3">
+        <div className="w-7/12">
         <p className="text-sm font-semibold text-neutral-700">{detail?.name}</p>
-        <p className="py-2 text-sm font-normal text-neutral-500">Passenger:  <b className="text-base text-neutral-900 uppercase">Anna Anderson</b></p>
-        <p className="text-sm font-normal text-tertiary2-750 pb-2">{travelClass}</p>
-        <div className="flex justify-between py-2">
+        <p className="py-1 md:py-2 text-xs md:text-sm font-normal text-neutral-500">Passenger:  <b className="text-xs md:text-base text-neutral-900 uppercase">Anna Anderson</b></p>
+        <p className="text-sm font-normal text-tertiary2-750 md:pb-2">{travelClass}</p>
+        <div className="flex justify-between items-center py-1 md:py-2">
           <div>
-            <p className="text-3xl text-secondary-500 font-medium">23:15</p>
-            <p className="text-sm font-medium text-neutral-900">{start}</p>
-            <p className="text-sm font-normal text-neutral-500">9 Feb, 2023</p>
+            <p className=" text-xl md:text-3xl text-secondary-500 font-medium">23:15</p>
+            <p className="text-xs md:text-sm font-medium text-neutral-900">{start}</p>
+            <p className="text-xs md:text-sm font-normal text-neutral-500">9 Feb, 2023</p>
           </div>
           <div>
-            <p className="text-3xl text-secondary-500 font-medium">1:25</p>
-            <p >{end}</p>
-            <p className="text-sm font-normal text-neutral-500">10 Feb, 2023</p>
+            <p className="text-xl md:text-3xl text-secondary-500 font-medium">1:25</p>
+            <p className="text-xs md:text-sm font-medium text-neutral-900">{end}</p>
+            <p className="text-xs md:text-sm font-normal text-neutral-500">10 Feb, 2023</p>
           </div>
         </div>
-        <hr className="border-secondary-500 mx-5" />
+        <hr className="border-secondary-500 mx-2 md:mx-5" />
         <div className="text-center">
         <p className="text-sm font-normal text-neutral-900 py-2">2h10m</p>
         </div>
         <p className="text-sm font-normal text-neutral-900">Check-in: <b>9th Feb 2023 at 21:20</b></p>
         </div>
-        <div className=" flex flex-col justify-between items-end text-right">
+        <div className=" flex flex-col h-full justify-between items-end text-right w-5/12">
           <p className="rounded-xl p-1 text-xs font-normal bg-neutral-300">{number}x23kg</p>
-          <div className="-rotate-90 -mr-14">
+          <div className="-rotate-90 -mr-9 md:-mr-14">
         <svg ref={svgRef}></svg>
         </div>
-          <p className="text-sm text-neutral-550 font-normal">*All time displayed are local</p>
+          <p className="text-xs md:text-sm text-neutral-550 font-normal">*All time displayed are local</p>
         </div>
         </div>
-        <div className="bg-primary-700 w-1/3 rounded-xl p-5">
+        <div className="bg-primary-700 w-1/3 rounded-xl pt-2 md:py-5 px-2 md:p-5">
         <div className="flex justify-center gap-5 items-center"> 
           <img src={logo} alt="" srcSet="" className="w-[10%]" />
         <p className="font-semibold text-xl text-white">E-flight</p>
         </div>
-        <div className="py-2">
+        <div className="py-1 md:py-2">
         <p className="text-sm py-1 text-neutral-550 font-medium">Booking ID</p>
         <p className="text-2xl text-neutral-550 font-medium">65980165 </p>
         </div>
@@ -97,7 +97,7 @@ fontSize = 8}:BarcodeProps) => {
           <p className="text-sm text-neutral-550 font-medium">Airline Booking Code</p>
           <p className="text-2xl text-neutral-550 font-medium">CA-6018</p>
         </div>
-        <div className="flex justify-center mt-5">
+        <div className="flex justify-center mt-1 md:mt-5">
         <svg ref={svgRef2}></svg>
        </div>
       </div>
--- src/pages/home/LandingPage.tsx
+++ src/pages/home/LandingPage.tsx
@@ -9,6 +9,7 @@ const LandingPage = () => {
         <NavBar/>
         <img src={dashboard} alt="" srcSet="" className="mt-16" />
       <Link to="/flights"> <button className="bg-secondary-500 text-white mt-5 font-bold px-5 py-3 rounded-xl">Get Stared</button></Link>
+      
        <Footer/>
       
     </BackgroundLayout>
