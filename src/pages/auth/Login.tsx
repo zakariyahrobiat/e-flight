c28a5bbc115459650f4df72920ca24a602e1ff0e
zakariyahrobiat <zakariyahrobiat2018@gmail.com>
2024-12-18 18:53:38 +0100

    fixed firebase sign in with google error

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -2,14 +2,13 @@ import Footer from "../../component/Footer"
 import NavBar from "../../component/NavBar"
 import BackgroundLayout from "../../Layout/BackgroundLayout"
 import InputContent from "../../component/input/InputContent"
-import { useAuth } from "../../hooks/useAuth"
 const Login = () => {
-  const {handleGoogleAuth} = useAuth()
+  const handleGoogleLogin=async()=>{}
   return (
     <BackgroundLayout>
       <NavBar/>
       <div className="mt-20 mx-5 md:w-1/2 md:mx-auto bg-neutral-150 shadow-3xl rounded p-5">
-      <InputContent heading="Sign in" button="Sign in" option="Login with your Google Account" discription="Need an account?" link="/register" text="Sign up here" onClick={handleGoogleAuth}/>
+      <InputContent heading="Sign in" button="Sign in" option="Login with your Google Account" discription="Need an account?" link="/register" text="Sign up here" onClick={handleGoogleLogin}/>
       </div>
       <Footer/>
     </BackgroundLayout>
